e4a00f6e38c714b2f195acdeacb05238132acd23
venkat <venkatsubbu89@gmail.com>
2016-02-12 00:42:27 -0800

    Movie.java setter and getter methods are defined MovieService methods are defined.

--- src/main/java/io/egen/rentalflix/Movie.java
+++ src/main/java/io/egen/rentalflix/Movie.java
@@ -1,9 +1,97 @@
 package io.egen.rentalflix;
 
+import java.util.UUID;
+
 /**
  * Entity representing a movie.
  * Fields: id, title, year, language
  */
 public class Movie {
-	//POJO IMPLEMENTATION GOES HERE
+	
+	private UUID id;
+	private String title;
+	private int year;
+	private String language;
+	private boolean isRented;
+	private String rentedUser;
+			
+	public String getRentedUser() {
+		return rentedUser;
+	}
+
+	public void setRentedUser(String rentedUser) {
+		this.isRented = true;
+		this.rentedUser = rentedUser;
+	}
+
+	public void resetRentedStatus(){
+		this.isRented = false;
+		this.rentedUser = "";
+	}
+	
+	public Movie(String title, int year, boolean isRented, UUID id, String language) {
+		super();
+		this.title = title;
+		this.year = year;
+		this.isRented = isRented;
+		this.id = id;
+		this.language = language;
+	}
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public boolean isRented() {
+		return isRented;
+	}
+	public void setRented(boolean isRented) {
+		this.isRented = isRented;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+		
 }
--- src/main/java/io/egen/rentalflix/MovieService.java
+++ src/main/java/io/egen/rentalflix/MovieService.java
@@ -1,9 +1,79 @@
 package io.egen.rentalflix;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 /**
  * Service implementing IFlix interface
  * You can use any Java collection type to store movies
  */
 public class MovieService implements IFlix {
+	
+	private static Map<UUID, Movie> movies;
+	private static Map<String, List<UUID>> nameToMoviesMap;
+	
+	static{
+		movies = new HashMap<UUID, Movie>();
+		nameToMoviesMap = new HashMap<String, List<UUID>>();
+	}
+		
+	@Override
+	public List<Movie> findAll() {	
+		List<Movie> existingMovies = (List<Movie>)movies.values();
+		return existingMovies;
+	}
+
+	@Override
+	public List<Movie> findByName(String name) {
+		List<UUID> movieIds = nameToMoviesMap.get(name);
+		List<Movie> moviesOfGivenName = new ArrayList<>();
+		for(UUID id : movieIds){
+			Movie movie = movies.get(id);
+			moviesOfGivenName.add(movie);
+		}
+		return moviesOfGivenName;
+	}
+
+	@Override
+	public Movie create(Movie movie) {	
+		movies.put(movie.getId(), movie);
+		if(nameToMoviesMap.containsKey(movie.getTitle())){
+			 List<UUID> currentIds =  nameToMoviesMap.get(movie.getTitle());
+			 currentIds.add(movie.getId());
+		}
+		else{
+			List<UUID> newId = new ArrayList<>();
+			newId.add(movie.getId());
+			nameToMoviesMap.put(movie.getTitle(), newId);
+		}
+		return movie;
+	}
+
+	@Override
+	public Movie update(Movie movie) {		
+		if(movies.containsKey(movie.getId())){
+			return movies.put(movie.getId(), movie);
+		}
+		return null;
+	}
+
+	@Override
+	public Movie delete(int id) {
+		
+		return movies.get(id);
+	}
+
+	@Override
+	public boolean rentMovie(int movieId, String user) {
+		Movie movie = movies.get(movieId);
+		if(!movie.isRented()){
+			movie.setRentedUser(user);			 //isRented is set inside the setRentedUser function
+			return true;
+		}
+		return false;
+	}
 
 }
